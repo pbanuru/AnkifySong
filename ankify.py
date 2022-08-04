5faea5fa804f502fb364f498afa8f97eb61a70e8
user <55062649+pbanuru@users.noreply.github.com>
2022-08-03 18:29:13 -0700

    Minimal Example working without media We created one note, and added it to our deck. Now I'll try adding media (one audio file)

--- ankify.py
+++ ankify.py
@@ -1,2 +1,30 @@
 import genanki
 import random
+
+
+def rand_id():
+    return random.randrange(1 << 30, 1 << 31)
+
+
+def model_setup():
+    fields = [{'name': 'Question'}, {'name': 'Answer'}]
+    templates = [
+        {
+            'name': 'Card 1',
+            'qfmt': '{{Question}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+        },
+    ]
+    return fields, templates
+
+
+def gen_model(title):
+    model_id = rand_id()
+    fields, template = model_setup()
+    model = genanki.Model(model_id, title, fields, template)
+    return model
+
+
+def gen_deck(title):
+    deck_id = rand_id()
+    return genanki.Deck(deck_id, title)
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import os
 
 
 def run():
-    pass
+    deck = gen_deck("Anki Deck")
+    model = gen_model("Anki Model")
+    note = genanki.Note(model, ["What is the time?", "6:25PM"])
+    deck.add_note(note)
+
+    # Generate package
+    genanki.Package(deck).write_to_file("data/anki_deck.apkg")
 
 
 if __name__ == '__main__':
