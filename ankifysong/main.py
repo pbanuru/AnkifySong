cf37c491f2a9fda2f03aad49f484f6e5e339f17b
pbanuru <55062649+pbanuru@users.noreply.github.com>
2024-04-24 23:57:04 -0700

    Restructure for pytest AnkifySong ├── ankifysong │   ├── LICENSE │   ├── README.md │   ├── __init__.py │   ├── ankify.py │   ├── data │   ├── gui.py │   ├── lyrics.srt │   ├── lyrics_annotated.srt │   ├── main.py │   ├── mp3Processor.py │   ├── requirements.txt │   ├── srtProcessor.py │   └── vocal-remover ├── setup.py ├── tests │   ├── __init__.py │   ├── test_ankify.py │   ├── test_gui.py │   ├── test_main.py │   ├── test_mp3Processor.py │   └── test_srtProcessor.py └── venv     └── ...

---
+++ ankifysong/main.py
@@ -0,0 +1,75 @@
+import ankify
+import os
+import sys
+import mp3Processor
+import srtProcessor
+import argparse
+
+REDOWNLOAD_CORE = False
+DEBUG = True
+
+def clear_data():
+    # Delete Core Data if REDOWNLOAD_CORE is True except for placeholder
+    if REDOWNLOAD_CORE:
+        for file in os.listdir("data/core"):
+            if file != ".placeholder":
+                os.remove(f"data/core/{file}")
+        
+    # Delete all files in clips folder except for placeholder
+    for file in os.listdir("data/clips"):
+        if file != ".placeholder":
+            os.remove(f"data/clips/{file}")
+    
+    # Delete all other files in data folder.
+    for file in os.listdir("data"):
+        if file != "core" and file != "clips":
+            os.remove(f"data/{file}")
+
+def run(link, name, srt_path="./lyrics.srt", output_path="data/anki_deck.apkg"):
+
+    clear_data() # Empty data folder of all files besides placeholder
+    
+    song = mp3Processor.Song(link)
+
+    # Parse SRT file
+    # Buffer set to 0.5 seconds by default, can be changed
+    srt_processor = srtProcessor.SrtProcessor(song, srt_path, 0.5)
+    note_field_lists, audio_paths = srt_processor.processSrtFile()
+
+    deck = ankify.gen_deck(name)
+    model = ankify.gen_model("My Model")
+
+    # Add notes to deck
+    ankify.add_notes(deck, model, note_field_lists)
+
+    # Generate package
+    ankify.gen_package(deck, audio_paths, output_path)
+
+if __name__ == '__main__':
+    if DEBUG:
+        run("https://www.youtube.com/watch?v=r6cIKA1SWI8", "Spinning Sky Rabbit")
+        exit(0)
+
+    parser = argparse.ArgumentParser(
+        description="Generate Anki flashcards from songs.",
+        epilog="""Examples:
+        Using Default lyrics.srt file:
+        python main.py "https://www.youtube.com/watch?v=r6cIKA1SWI8" "Spinning Sky Rabbit"
+        
+        To specify srt path:
+        python main.py "https://www.youtube.com/watch?v=r6cIKA1SWI8" "Spinning Sky Rabbit" -s .../.../mylyrics.srt""",
+        formatter_class=argparse.RawDescriptionHelpFormatter  # This ensures that the epilog is formatted as provided
+    )
+    
+    # Required arguments
+    parser.add_argument("youtube_link", help="YouTube link of the song.")
+    parser.add_argument("deck_name", help="Name of the Anki deck.")
+    
+    # Optional arguments
+    parser.add_argument("-s", "--srt_path", default="./lyrics.srt", help="Path to the SRT file. Default is ./lyrics.srt.")
+    parser.add_argument("-o", "--output_path", default="data/anki_deck.apkg", help="Path to save the generated Anki deck. Default is data/anki_deck.apkg.")
+    
+    args = parser.parse_args()
+
+    run(args.youtube_link, args.deck_name, args.srt_path, args.output_path)
+
---
+++ ankifysong/mp3Processor.py
@@ -0,0 +1,80 @@
+import pytube
+import moviepy.editor as mp
+import os
+import shutil
+
+class Song:
+    """
+    Song class to organize song data
+    """
+
+    def __init__(self, url):
+        self.url = url
+        self.download()
+        self.isolate_vocals() # Remove vocals from source audio and generate audio_Vocals.wav and audio_Instruments.wav
+        self.AUDIO_SRC_CLIP = mp.AudioFileClip("data/core/audio.aac")
+        self.AUDIO_VOCALS_CLIP = mp.AudioFileClip("data/core/audio_Vocals.wav")
+
+    def download(self):
+        """
+        Download song from Youtube URL
+        """
+
+        # Check for existing audio file in data folder
+        if os.path.exists("data/core/audio.aac"):
+            print("Audio already downloaded")
+            return
+
+        yt = pytube.YouTube(self.url)
+        title = yt.title
+
+        print(f'Downloading {title} ...')
+
+        # Download the file
+        yt.streams.filter(only_audio=True).first().download(
+            filename='data/core/audio.aac')
+
+        print(f'Download Complete: {title} ✨')
+
+    def isolate_vocals(self):
+
+        # Check if vocal data/audio_Vocals.wav or data/audio_Instruments.wav exists
+        if os.path.exists("data/core/audio_Vocals.wav") and os.path.exists("data/core/audio_Instruments.wav"):
+            print("Vocals already extracted")
+            return
+            
+        # Call vocal remover 
+        print("Extracting Pure Vocals...")
+        os.system("cd vocal-remover && python inference.py --input ../data/core/audio.aac --gpu 0")
+        # subprocess.run(["cd", "vocal-remover", "&&", "python", "inference.py", "--input", "../data/core/audio.aac", "--gpu", "0"])
+        
+        # Move vocal-remover/audio_Vocals.wav to data/audio_Vocals.wav
+        shutil.move("vocal-remover/audio_Vocals.wav", "data/core/audio_Vocals.wav")
+        shutil.move("vocal-remover/audio_Instruments.wav", "data/core/audio_Instruments.wav")
+
+    def clip_timestamp(self, start, end, filename):
+        """
+        Download a subclip of the audio file as an mp3, giving it the name filename.mp3
+        """
+        # Use main audio file
+        clip_src = self.AUDIO_SRC_CLIP.subclip(start, end)
+
+        clip_src_name = f"{filename}_src.mp3"
+        clip_src_path = f"data/clips/{clip_src_name}"
+        clip_src.write_audiofile(clip_src_path)
+
+        # Use vocals only
+        clip_vocals = self.AUDIO_VOCALS_CLIP.subclip(start, end)
+
+        clip_vocals_name = f"{filename}_vocals.mp3"
+        clip_vocals_path = f"data/clips/{clip_vocals_name}"
+        clip_vocals.write_audiofile(clip_vocals_path)
+
+        return clip_src_name, clip_src_path, clip_vocals_name, clip_vocals_path
+
+    def song_length(self):
+        """
+        Returns the length of the audio file in seconds
+        """
+        return self.AUDIO_SRC_CLIP.duration
+        
